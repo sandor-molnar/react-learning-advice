badef68da50c87da6ef4a1fc9220e54b8e1b0625
Molnár Sándor Benjámin <molnar.sandor.benjamin@gmail.com>
2024-12-26 16:53:52 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import {useEffect, useState} from "react";
+
+export default function App() {
+    // state for the advice text
+    const [advice, setAdvice] = useState("");
+    // state for the number of advice gets
+    const [count, setCount] = useState(0);
+
+    // local function for fetching advice from an API
+    async function getAdvice() {
+        // wait for the fetch result before we progress futher
+        const res = await fetch('https://api.adviceslip.com/advice');
+        // convert the response to json
+        const data = await res.json();
+
+        // using the "useState" setAdvice to set the "advice" variable value
+        setAdvice(data.slip.advice);
+        // Same here with incrementing the state value without accessing the "count" variable
+        setCount((prevState) => prevState + 1);
+    }
+
+    // useEffect will run the code inside when the page load
+    useEffect(function () {
+        getAdvice();
+    }, []); // We need to set the dependency to an empty array so it not constantly running
+
+    // Content
+    return <div>
+        <h1>{advice}</h1>
+        <button onClick={getAdvice}>Get advice</button>
+        <p>You have read <strong>{count}</strong> pieces of advice</p>
+    </div>
+}
